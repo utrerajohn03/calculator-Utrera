d0721c41e49a1bad3b4a27197063787d22137b95
utrerajohn03 <pamelakang44@gmail.com>
2026-02-19 22:43:48 +0800

    [feat] for calculator

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('display');
+
+function appendValue(val) {
+    display.value += val;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
